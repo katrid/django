1bef1a99d935845135ab834030ae222c7bbf9404
Ari Caldeira <ari@erpintegra.com.br>
2015-05-27 16:40:58 -0300

    Adding customized glibc locales, in order to correct the sorting order for Brazilian Portuguese

--- django/conf/locale/i18n/generate-locales.sh
+++ django/conf/locale/i18n/generate-locales.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 localedef -i pt_BR -c -f UTF-8 ../pt_BR
-localedef -i en_US -c -f UTF-8 ../en_US
+localedef -i en_US -c -f UTF-8 ../en
 # localedef -i eo_BR -c -f UTF-8 ../eo_BR
 # localedef -i en_GB -c -f UTF-8 ../en_GB
 # localedef -i en_CA -c -f UTF-8 ../en_CA
